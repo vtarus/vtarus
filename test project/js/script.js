e4fe40166ccd3e29b27a6ef6e3caaa8f8068de76
Eugene <vtarus@yandex.ru>
2021-07-11 21:15:22 +0300

    firt test

---
+++ test project/js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+    //incr++;
+   // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+
+    const isChecked = true,
+          isClose = true;
+          
+    console.log(isChecked && isClose);
